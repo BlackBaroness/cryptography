3ad7f14f4fdaae5a25e10e63daca13015189769e
BlackBaroness <shoyurash2@gmail.com>
2022-09-05 01:13:00 +0300

    Specify UTF-8 charset during tests

--- src/test/java/com/github/blackbaroness/cryptography/test/TestHelper.java
+++ src/test/java/com/github/blackbaroness/cryptography/test/TestHelper.java
@@ -4,6 +4,8 @@ import com.github.blackbaroness.cryptography.hashing.algorithm.HashAlgorithm;
 import com.github.blackbaroness.cryptography.hashing.source.HashSources;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class TestHelper {
@@ -11,6 +13,6 @@ public final class TestHelper {
     public static void testSimpleHashing(
             @NotNull HashAlgorithm algorithm, @NotNull String exampleString, @NotNull String expectedResult
     ) {
-        assertEquals(expectedResult, algorithm.hash(HashSources.ofString(exampleString)).string());
+        assertEquals(expectedResult, algorithm.hash(HashSources.ofString(exampleString, StandardCharsets.UTF_8)).string());
     }
 }
